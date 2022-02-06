f35eadd9b9cda83c89714aa26dd09517fcf5681c
Vasil Hayhel <89933931+hayhelv@users.noreply.github.com>
2022-02-06 18:23:29 +0200

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryConteiner = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItems(galleryItems);
+
+galleryConteiner.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryConteiner.addEventListener('click', onGalleryConteinerClick);
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи.
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<div class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+                </a>
+                </div>`;
+    }).join('');
+}
+
+function onGalleryConteinerClick(e) {
+    e.preventDefault();
+    
+    if (e.target.nodeName !== 'IMG') {
+    return;
+    }
+    modalShow(e.target.dataset.source);
+    
+}
+
+function modalShow(src) {
+     const instance = basicLightbox.create(`<img src="${src}" width="800" height="600">`)
+
+    instance.show()
+}
